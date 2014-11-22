23f2b5e9d4464bfdc8138ec3ff2f24e721216b7c
Andreas Reich (@LAPTOP) <r_andreas2@web.de>
2014-11-22 23:26:07 +0100

    neeeew level!

---
+++ solutions/4.js
@@ -0,0 +1,5 @@
+  draw.triangle(new Pos(150, 100), new Pos(250, 250), new Pos(50, 250), "#00AA00");
+  draw.triangle(new Pos(150, 50), new Pos(225, 175), new Pos(75, 175), "#00CC00");
+  draw.triangle(new Pos(150, 0), new Pos(190, 85), new Pos(110, 85), "#00FF00");
+  draw.rectangle(new Pos(130, 250), 40, 50, "#FF9001");
+  if(false)
